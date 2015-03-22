5de749608a9932ce13a23672b0692fa4f913b617
Hunter Chung <b89603112@gmail.com>
2015-03-22 12:58:26 -0700

    bug fixes

--- install_python_packages.sh
+++ install_python_packages.sh
@@ -7,23 +7,23 @@ pip install -U \
     fisher \
     numexpr \
     bottleneck \
-    # Has trouble to install lxml
-    # lxml \
     html5lib \
     beautifulsoup4 \
     MACS2 \
     scipy \
     pandas \
     scikit-learn \
-    # ceas \
     cutadapt
+    # Has trouble to install lxml
+    # lxml \
+    # ceas \
 
 # pytbles needs to be installed after numexpr.
 pip install \
-    # rpy2 \
     tables \
     ipython \
     matplotlib \
     seaborn \
     matplotlib-venn \
     qiime
+    # rpy2 \
