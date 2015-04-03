09753dbc7967bea64fa4369125e18b172a632743
Hunter Chung <b89603112@gmail.com>
2015-04-02 17:33:33 -0700

    add auto update to mysql

--- migration/migrate.sh
+++ migration/migrate.sh
@@ -5,7 +5,7 @@ mysql -e "create database current; create database migration"
 mysqldump -h $DB_HOST_NAME -u $DB_USERNAME --password=$DB_PASSWORD epiquest | mysql current
 python /usr/share/EpiQuest_py/manage.py migrate
 python /usr/share/EpiQuest_py/analysis/migrate_to_EG.py
-
+mysqldump migration | mysql -h $DB_HOST_NAME -u $DB_USERNAME --password=$DB_PASSWORD EG_migration
 unset DB_HOST_NAME
 unset DB_USERNAME
 unset DB_PASSWORD
